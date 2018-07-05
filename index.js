072b3a66ce893850f66dce650e5ddec5d0dc4a80
Allan Browning <allan@inov8design.net>
2018-07-05 12:51:23 +1000

    fix bad merge

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function addnums(numa,numb){
   return result;
 
 }
-<<<<<<< HEAD
 
-=======
 console.log("Addtion of two nums = " + addnums(10,5));
->>>>>>> ffa0ff6b731215f2a32afed7ce92311ce4f3a888
+
